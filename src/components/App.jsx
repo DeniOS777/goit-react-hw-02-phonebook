aa5b7567d342b7e82961545ffdee2433af331232
DeniOS777 <Denios777@gmail.com>
2022-07-31 16:40:59 +0300

    update foles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,10 @@ import { nanoid } from 'nanoid';
 class App extends Component {
   state = {
     contacts: [
-      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
     name: '',
@@ -109,9 +109,9 @@ class App extends Component {
           value={filter}
           onChange={this.handleChange}
         />
-        {filteredContacts.map(contact => (
-          <li key={contact.name}>
-            {contact.name}: {contact.number}
+        {filteredContacts.map(({ id, name, number }) => (
+          <li key={id}>
+            {name}: {number}
             <button type="button">Delete</button>
           </li>
         ))}
