26f016b6badc24e2a1d77b231570b170d0740f87
DeniOS777 <Denios777@gmail.com>
2022-07-31 15:46:44 +0300

    add field phone number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,36 +5,39 @@ class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
   inputNameId = nanoid();
+  inputNumberId = nanoid();
 
-  addContact = name => {
+  addContact = ({ name, number }) => {
     const newContact = {
       id: nanoid(),
       name,
+      number,
     };
 
-    this.setState(prevState => {
-      return {
-        contacts: [newContact, ...prevState.contacts],
-      };
-    });
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
   };
 
-  handleChange = e => this.setState({ name: e.target.value });
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
   handleSubmit = e => {
-    const { name } = this.state;
     e.preventDefault();
-    this.addContact(name);
+    this.addContact(this.state);
     this.resetForm();
   };
 
-  resetForm = () => this.setState({ name: '' });
+  resetForm = () => this.setState({ name: '', number: '' });
 
   render() {
-    const { contacts, name } = this.state;
+    const { contacts, name, number } = this.state;
 
     return (
       <div style={{ padding: '10px' }}>
@@ -55,8 +58,9 @@ class App extends Component {
             Name
           </label>
           <input
-            style={{ marginBottom: '15px' }}
+            style={{ marginBottom: '20px' }}
             id={this.inputNameId}
+            placeholder="Add contact..."
             type="text"
             name="name"
             value={name}
@@ -65,12 +69,27 @@ class App extends Component {
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
+          <label style={{ marginBottom: '5px' }} htmlFor={this.inputNumberId}>
+            Number
+          </label>
+          <input
+            style={{ marginBottom: '20px' }}
+            id={this.inputNumberId}
+            placeholder="Add phone number..."
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
         {contacts.map(contact => (
           <li key={contact.id}>
-            {contact.name}
+            {contact.name}: {contact.number}
             <button type="button">Delete</button>
           </li>
         ))}
