3122afdf9431bf906c7e7386d579bbd8582be4f3
DeniOS777 <Denios777@gmail.com>
2022-07-31 17:09:34 +0300

    change ContactForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
 
 class App extends Component {
   state = {
@@ -10,12 +11,7 @@ class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
   };
-
-  inputNameId = nanoid();
-  inputNumberId = nanoid();
 
   addContact = ({ name, number }) => {
     const newContact = {
@@ -37,19 +33,6 @@ class App extends Component {
     );
   };
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.addContact(this.state);
-    this.resetForm();
-  };
-
-  resetForm = () => this.setState({ name: '', number: '', filter: '' });
-
   render() {
     const { contacts, filter, name, number } = this.state;
     const filteredContacts = this.filterContacts();
@@ -57,50 +40,7 @@ class App extends Component {
     return (
       <div style={{ padding: '10px' }}>
         <h1 style={{ marginBottom: '20px' }}>Phonebook</h1>
-        <form
-          onSubmit={this.handleSubmit}
-          autoComplete="off"
-          style={{
-            display: 'inline-flex',
-            flexDirection: 'column',
-            marginBottom: '20px',
-            padding: '20px',
-            width: '350px',
-            border: '1px solid #000',
-          }}
-        >
-          <label style={{ marginBottom: '5px' }} htmlFor={this.inputNameId}>
-            Name
-          </label>
-          <input
-            style={{ marginBottom: '20px' }}
-            id={this.inputNameId}
-            placeholder="Enter contact..."
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <label style={{ marginBottom: '5px' }} htmlFor={this.inputNumberId}>
-            Number
-          </label>
-          <input
-            style={{ marginBottom: '20px' }}
-            id={this.inputNumberId}
-            placeholder="Enter number..."
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-          <button type="submit">Add contact</button>
-        </form>
+        <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
         <p>Find contacts by name</p>
         <input
