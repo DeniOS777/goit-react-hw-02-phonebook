334f4764d3198febdcdbb3ca9bd0b4b579ba34df
DeniOS777 <Denios777@gmail.com>
2022-07-31 12:52:18 +0300

    add nanoid npm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,25 @@
-export const App = () => {
-  return <div></div>;
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </form>
+    );
+  }
+}
+
+export default App;
